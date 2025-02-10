5266a2c3c869918e6c5cfe1e229261080f90a871
jyy <1305081045@qq.com>
2025-02-10 12:35:12 +0000

    feature: implement chapter06 issue 39 by using stack

--- chapter06/_39_largest_rectangle_area.rs
+++ chapter06/_39_largest_rectangle_area.rs
@@ -45,6 +45,42 @@ fn largest_rectangle_area_divide_and_conquer_version(heights: Vec<i32>) -> i32 {
     helper(heights.as_slice(), 0, heights.len())
 }
 
+fn largest_rectangle_area_stack_version(heights: Vec<i32>) -> i32 {
+    let mut area = 0;
+    let mut stack = std::collections::VecDeque::new();
+    for i in heights.iter().enumerate() {
+            // 栈顶的元素大于当前元素
+        while  !stack.is_empty() && *heights.get( *stack.back().unwrap()).unwrap() > *i.1 { 
+            let stack_peek = stack.pop_back().unwrap() as i32;
+            let rectangle_height = heights.get(stack_peek as usize).unwrap();
+            // 计算以rectangle_height为高的面积       
+            let left_index = stack.back();
+            let right_index = i.0 as i32;
+            let width;
+            if let Some(left) = left_index {
+                width = right_index - (*left) as i32 - 1; 
+            } else {
+                width = stack_peek - 1;    
+            }
+            area = area.max(width * (*rectangle_height));
+        }
+        stack.push_back(i.0);
+            
+    }
+    while !stack.is_empty() {
+        let stack_peek = stack.pop_back().unwrap() as i32;
+        let rectangle_height = heights.get(stack_peek as usize).unwrap();
+        let left_index = match stack.back() {
+            Some(r) => { (*r) as i32},
+            None => stack_peek - 1 
+        };
+        let right_index = stack_peek + 1;
+        let width = right_index - left_index - 1;
+        
+        area = area.max(width * (*rectangle_height));
+    }
+    area
+}
 
 fn main() {
     let v = vec![3, 2, 5, 4, 6, 1, 4, 2];
@@ -52,6 +88,9 @@ fn main() {
     println!("r={:?}", r);
     assert_eq!(r, 12);
     let r = largest_rectangle_area_divide_and_conquer_version(v.clone());
+    println!("r={:?}", r);
+    assert_eq!(r, 12);
+    let r = largest_rectangle_area_stack_version(v.clone());
     println!("r={:?}", r);
     assert_eq!(r, 12);
 }
